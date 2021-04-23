f1dbb91986809051c341485fe507a22a60113827
Saulo Marcos Corrêa <saulo@tinotech.com.br>
2021-04-22 22:41:56 -0300

    Adicionado estrutura para Messages

---
+++ src/controllers/MessagesController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { MessagesService } from "../services/MessagesService";
+
+class MessagesController {
+    async create(request: Request, response: Response) {
+        const { admin_id, text, user_id } = request.body;
+        const messagesService = new MessagesService();
+
+        try {
+            
+            const message = await messagesService.create({ 
+                admin_id,
+                text,
+                user_id
+            });
+            return response.json(message);
+
+        } catch (err) {
+            return response
+                .status(400)
+                .json({
+                    message: err.message
+                });
+        }
+    }
+}
+
+export { MessagesController };
